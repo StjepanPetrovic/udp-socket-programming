9612048b9c8dadbc8a3f23f0b8d2526b4fc6a5d7
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-10-12 18:07:35 +0200

    Add logic for receiving data from client and sending response to client

--- SocketServer.php
+++ SocketServer.php
@@ -13,3 +13,21 @@ if (socket_bind($socket, $serverIpAddr, $serverPort)) {
     echo "Error: " . socket_strerror(socket_last_error($socket)) . "\n";
     exit(1);
 }
+
+while (true) {
+    $clientIpAddr = '';
+    $clientPort = 0;
+
+    if (socket_recvfrom($socket, $data, 1024, 0, $clientIpAddr, $clientPort)) {
+        echo "Received $data from $clientIpAddr:$clientPort\n";
+        if (socket_sendto($socket, "OK\n", 3, 0, $clientIpAddr, $clientPort)) {
+            echo "Sent OK to $clientIpAddr:$clientPort\n";
+        } else {
+            echo "Error: " . socket_strerror(socket_last_error($socket)) . "\n";
+            exit(1);
+        }
+    } else {
+        echo "Error: " . socket_strerror(socket_last_error($socket)) . "\n";
+        exit(1);
+    }
+}
