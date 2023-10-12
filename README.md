6c5a7d34ac74e5f38c25b5b1f8dfdfe0f389380e
Stjepan Petrovic <stjepanpetrovic11@gmail.com>
2023-10-12 17:37:49 +0200

    Add checking for errors during binding and echo status message

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 `socket_create` https://www.php.net/manual/en/function.socket-create.php
 `socket_bind` https://www.php.net/manual/en/function.socket-bind.php
+`socket_strerror` https://www.php.net/manual/en/function.socket-strerror.php
+`socket_last_error` https://www.php.net/manual/en/function.socket-last-error.php
--- SocketServer.php
+++ SocketServer.php
@@ -7,4 +7,9 @@ $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
 $serverIpAddr = '0.0.0.0';
 $serverPort = 1234;
 
-socket_bind($socket, $serverIpAddr, $serverPort);
+if (socket_bind($socket, $serverIpAddr, $serverPort)) {
+    echo "Server is listening on $serverIpAddr:$serverPort\n";
+} else {
+    echo "Error: " . socket_strerror(socket_last_error($socket)) . "\n";
+    exit(1);
+}
